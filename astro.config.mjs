e82a2d8ddd7009d4de3f15139a669d2d19a46020
Simon Mau <git@simonmau.me>
2024-05-04 15:41:29 +0800

    feat: tailwind; perf: componentization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://simonxmau.github.io',
-    integrations: [preact()],
-    server: {
-        host: true,
-        port: 8019,
-    }
+  site: 'https://simonxmau.github.io',
+  integrations: [
+      preact(),
+      tailwind({
+          applyBaseStyles: false,
+          nesting: true,
+      }),
+  ],
+  server: {
+    host: true,
+    port: 8019
+  }
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,101 @@
+/** @type {import('tailwindcss').Config} */
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      // 'blue': '#1fb6ff',
+      // 'purple': '#7e5bef',
+      // 'pink': '#ff49db',
+      // 'orange': '#ff7849',
+      // 'green': '#13ce66',
+      // 'yellow': '#ffc82c',
+      // 'gray-dark': '#273444',
+      // 'gray': '#8492a6',
+      // 'gray-light': '#d3dce6',
+    },
+    screens: {
+      // sm: '480px',
+      // md: '768px',
+      // lg: '976px',
+      // xl: '1440px',
+    },
+    fontFamily: {
+      // sans: ['Graphik', 'sans-serif'],
+      // serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      textColor: {
+        skin: {
+          base: withOpacity("--color-text-base"),
+          accent: withOpacity("--color-accent"),
+          inverted: withOpacity("--color-fill"),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: withOpacity("--color-fill"),
+          accent: withOpacity("--color-accent"),
+          inverted: withOpacity("--color-text-base"),
+          card: withOpacity("--color-card"),
+          "card-muted": withOpacity("--color-card-muted"),
+        },
+      },
+      outlineColor: {
+        skin: {
+          fill: withOpacity("--color-accent"),
+        },
+      },
+      borderColor: {
+        skin: {
+          line: withOpacity("--color-border"),
+          fill: withOpacity("--color-text-base"),
+          accent: withOpacity("--color-accent"),
+        },
+      },
+      fill: {
+        skin: {
+          base: withOpacity("--color-text-base"),
+          accent: withOpacity("--color-accent"),
+        },
+        transparent: "transparent",
+      },
+      fontFamily: {
+        mono: ["IBM Plex Mono", "monospace"],
+      },
+
+      typography: {
+        DEFAULT: {
+          css: {
+            pre: {
+              color: false,
+            },
+            code: {
+              color: false,
+            },
+          },
+        },
+      },
+
+      // spacing: {
+      //   '128': '32rem',
+      // },
+      // width: {
+      //   '128': '3rem',
+      // }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
+
